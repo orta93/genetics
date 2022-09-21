80bde74a9e9980e6c696285359ed9658d0c4b1ff
Jonathan Orta <emanuelorta93@hotmail.com>
2022-09-21 18:03:07 -0500

    Replace box kit

--- resources/views/saludable/acerca-de-pruebas.blade.php
+++ resources/views/saludable/acerca-de-pruebas.blade.php
@@ -13,25 +13,25 @@
         $options = [
             [
                 'title' => env('APP_NAME').' Detección de Cáncer',
-                'icon' => 'box-angle-productcard.webp',
+                'icon' => 'BOX_KIT_BG.PNG',
                 'bgColor' => 'bg-green-300',
                 'description' => 'Analiza 61 genes asociados con cánceres comunes, incluidos el cáncer de mama, ovario y próstata.'
             ],
             [
                 'title' => env('APP_NAME').' Cardio Screening',
-                'icon' => 'box-angle-productcard.webp',
+                'icon' => 'BOX_KIT_BG.PNG',
                 'bgColor' => 'bg-pink-900',
                 'description' => 'Analiza más de 75 genes asociados con formas genéticas de enfermedad cardíaca, incluido el agrandamiento del corazón, el ritmo cardíaco anormal y el colesterol alto.'
             ],
             [
                 'title' => 'Examen de salud genética de '.env('APP_NAME'),
-                'icon' => 'box-angle-productcard.webp',
+                'icon' => 'BOX_KIT_BG.PNG',
                 'bgColor' => 'bg-indigo-500',
                 'description' => 'Analiza hasta 147 genes, incluidos todos los genes en las pruebas de detección de cáncer y cardio, así como algunas afecciones hereditarias procesables.'
             ],
             [
                 'title' => 'Panel de Farmacogenómica de '.env('APP_NAME'),
-                'icon' => 'box-angle-productcard.webp',
+                'icon' => 'BOX_KIT_BG.PNG',
                 'bgColor' => 'bg-green-800',
                 'description' => 'Pruebe 38 genes para evaluar cómo sus genes afectan su respuesta a los medicamentos.'
             ]
